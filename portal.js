ec52529c8e590f7819043181f943098a58d7119d
JaniqueF <100723770@unimail.derby.ac.uk>
2026-04-30 11:31:06 +0100

    3) Signup & Inquiry
    - partially completed. need token validation and fake properties manually added to portal as test. (Need to implement adding actually properties)

--- portal.js
+++ portal.js
@@ -1,3 +1,4 @@
+//Login
 let loginBox = document.getElementById("loginBox");
 let message = document.getElementById("message");
 let button = document.getElementById("loginButton");
@@ -13,9 +14,12 @@ button.addEventListener("click", function (event) {
     console.log(userEmail);
     console.log(userPassword);
 
-    if (userEmail === user[0].email && userPassword === user[0].password) {
+    let matchedUser = user.find(function(account) {
+        return account.email === userEmail && account.password === userPassword;
+    });
+
+    if (matchedUser) {
         console.log("login success");
-        message.textContent = "Login successful";
         loginBox.style.display = "none";
         dashboard.style.display = "block";
 
@@ -23,4 +27,83 @@ button.addEventListener("click", function (event) {
         console.log("login fail");
         message.textContent = "Invalid credentials";
     }
+
+});
+
+//SignUp
+let showSignUp = document.getElementById("showSignUp");
+let signUpBox = document.getElementById("signUpBox");
+let backToLogin = document.getElementById("backToLogin");
+let createAccount = document.getElementById("createAccount");
+let fullName = document.getElementById("fullName");
+let signUpEmail = document.getElementById("signUpEmail");
+let signUpPassword = document.getElementById("signUpPassword");
+let confirmPassword = document.getElementById("confirmPassword");
+let signUpMessage = document.getElementById("signUpMessage");
+
+showSignUp.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("clicked");
+    loginBox.style.display = "none";
+    signUpBox.style.display = "block";
+
+});
+
+createAccount.addEventListener("click", function() {
+    console.log("signup link clicked");
+    let name = fullName.value;
+    let email = signUpEmail.value;
+    let password = signUpPassword.value;
+    let confirm = confirmPassword.value;
+
+    if (name === "" || email === "" || password === "" || confirm === "") {
+        signUpMessage.textContent = "Please complete all fields.";
+    }
+    else if (password.length < 8) {
+        signUpMessage.textContent = "Password must have a minimum of 8 characters.";
+
+    }
+
+    else if (password !== confirm) {
+        signUpMessage.textContent = "Passwords do not match.";
+
+    }
+
+    else {
+        signUpMessage.textContent = "Account created successfully!";
+        user.push({
+            fullName: name,
+            email: email,
+            password: password,
+            role: "customer"
+        });
+
+        signUpBox.style.display = "none";
+        loginBox.style.display = "block";
+    }
+
+});
+
+backToLogin.addEventListener("click", function () {
+    signUpBox.style.display ="none";
+    loginBox.style.display = "block";
+});
+
+
+let inquiryButton = document.getElementById("inquiryButton");
+let inquiryProperty = document.getElementById("inquiryProperty");
+let inquiryMessage = document.getElementById("inquiryMessage");
+let inquiryResult = document.getElementById("inquiryResult");
+
+inquiryButton.addEventListener("click",function() {
+    let property = inquiryProperty.value;
+    let message = inquiryMessage.value;
+    if (property === "" || message === "") {
+    inquiryResult.textContent = "Please fill in all fields";
+    } else {
+    inquiryResult.textContent = "Inquiry submitted successfully";
+    }
+
+
+
 });
